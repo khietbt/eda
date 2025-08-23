31b8181bbb329473e7d5644ba96a9c1a66ab06b3
khietbt <khietbt@gmail.com>
2025-08-23 16:58:27 +0700

    Protect the config service by OAuth2

---
+++ services/config-service/src/main/java/io/github/khietbt/eda/services/configservice/configurations/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package io.github.khietbt.eda.services.configservice.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/actuator/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
