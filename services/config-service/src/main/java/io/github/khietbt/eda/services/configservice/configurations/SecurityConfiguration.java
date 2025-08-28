358c7622fe7e09aa79c17d17f9a8ed636d894931
khietbt <khietbt@gmail.com>
2025-08-28 23:18:50 +0700

    Add keycloak-service

--- services/config-service/src/main/java/io/github/khietbt/eda/services/configservice/configurations/SecurityConfiguration.java
+++ services/config-service/src/main/java/io/github/khietbt/eda/services/configservice/configurations/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,10 +14,10 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
-                (authz) -> authz.requestMatchers("/actuator/**").permitAll()
+                auth -> auth.requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-                .csrf(csrf -> csrf.disable());
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
