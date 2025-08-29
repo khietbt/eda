56727f5b2e41e788ef35ce2ab083c19c9381663f
khietbt <khietbt@gmail.com>
2025-08-30 00:13:52 +0700

    Configure service-discovery-service to access config-service using oauth2

--- services/service-discovery-service/src/main/java/io/github/khietbt/eda/services/servicediscoveryservice/ServiceDiscoveryServiceApplication.java
+++ services/service-discovery-service/src/main/java/io/github/khietbt/eda/services/servicediscoveryservice/ServiceDiscoveryServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class ServiceDiscoveryService {
+public class ServiceDiscoveryServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ServiceDiscoveryService.class, args);
+        SpringApplication.run(ServiceDiscoveryServiceApplication.class, args);
     }
 }
