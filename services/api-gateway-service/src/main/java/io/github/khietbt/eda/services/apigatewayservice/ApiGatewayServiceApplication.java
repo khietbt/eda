b127a0c9ebb2cad9c06298e309e582a6027c0b14
khietbt <khietbt@gmail.com>
2025-08-30 00:44:40 +0700

    Update api-gateway-service to access config-service using oauth2

--- services/api-gateway-service/src/main/java/io/github/khietbt/eda/services/apigatewayservice/ApiGatewayServiceApplication.java
+++ services/api-gateway-service/src/main/java/io/github/khietbt/eda/services/apigatewayservice/ApiGatewayServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGatewayService {
+public class ApiGatewayServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayService.class, args);
+        SpringApplication.run(ApiGatewayServiceApplication.class, args);
     }
 }
